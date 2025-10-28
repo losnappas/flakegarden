a4ed6f69781f006aec96caac6e60cd7e5730f1d0
losnappas <hanu6@hotmail.com>
2025-10-28 12:09:54 +0200

    tool_nvidia: fix cudatoolkit import, add unfree declaration comment

--- templates/tool_nvidia.nix
+++ templates/tool_nvidia.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }:
 {
+  # TODO(@agent): allow non-free packages:
+  #
+  # Add the following inside `perSystem` in `flake.nix`:
+  #
+  # _module.args.pkgs = import inputs.nixpkgs {
+  #   inherit system;
+  #   config.allowUnfree = true;
+  # };
+
   make-shells.nvidia = {
     env = {
       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-        pkgs.cudaToolkit
+        pkgs.cudatoolkit
         pkgs.cudaPackages.cudnn
         "/run/opengl-driver"
       ];
