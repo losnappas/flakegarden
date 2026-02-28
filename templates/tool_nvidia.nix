a5470b7d836c2a7b0bb90096ccc3334472b65e8d
losnappas <hanu6@hotmail.com>
2026-02-28 10:54:17 +0200

    nvidia: update nix cache url
    It changed, according to https://nixos.wiki/wiki/CUDA, and seems to work.

--- templates/tool_nvidia.nix
+++ templates/tool_nvidia.nix
@@ -14,9 +14,11 @@
   # nixConfig = {
   #   extra-substituters = [
   #     "https://nix-community.cachix.org"
+  #     "https://cache.nixos-cuda.org"
   #   ];
   #   extra-trusted-public-keys = [
   #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #     "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
   #   ];
   # };
   #
